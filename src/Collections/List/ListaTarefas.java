f9767f5d887cce8d762c40732f88da9cf56c74b3
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-05-10 14:19:41 -0300

    Operações básicas com List

---
+++ src/Collections/List/ListaTarefas.java
@@ -0,0 +1,47 @@
+package Collections.List;
+
+import java.util.ArrayList;
+
+public class ListaTarefas {
+    private final ArrayList<Tarefa> tarefas;
+
+    public ListaTarefas() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        this.tarefas.add(new Tarefa(descricao.toLowerCase()));
+    }
+
+    public void removerTarefa(String descricao){
+        for (Tarefa tarefa : this.tarefas) {
+            if(tarefa.descricao().toLowerCase().equals(descricao)){
+                this.tarefas.remove(tarefa);
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("Tarefa não encontrada para esta descrição.");
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return this.tarefas.size();
+    }
+
+    public ArrayList<String> obterDescricoesTarefas(){
+
+        if(this.tarefas.isEmpty()){
+            throw new IllegalArgumentException("Lista de tarefas vazia");
+        }
+
+        ArrayList<String> descricoes = new ArrayList<>();
+
+        for (Tarefa tarefa : this.tarefas) {
+            descricoes.add(tarefa.descricao());
+        }
+
+        return descricoes;
+    }
+}
+
+record Tarefa(String descricao){ }
